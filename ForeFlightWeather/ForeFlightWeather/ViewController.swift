c6f7281bda18385edaff7691ad192074191ea00e
Christopher Williams <chrsw49@gmail.com>
2024-04-15 10:57:35 -0600

    Added UISearchBar with default text

--- ForeFlightWeather/ForeFlightWeather/ViewController.swift
+++ ForeFlightWeather/ForeFlightWeather/ViewController.swift
@@ -9,10 +9,13 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var airportSearchBar: UISearchBar!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        airportSearchBar.text = "KPWM"
     }
 
     override func didReceiveMemoryWarning() {
